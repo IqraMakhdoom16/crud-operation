149143c1509a4ae03a46977b276ee7cd0a790d6c
IqraMakhdoom16 <168168115+IqraMakhdoom16@users.noreply.github.com>
2024-05-18 15:13:01 +0500

    columns

--- src/components/StudentTable.jsx
+++ src/components/StudentTable.jsx
@@ -1,42 +1,43 @@
 import React from "react";
 import { Table, Button, Popconfirm } from "antd";
 
-const columns = (handleDelete, handleEdit) => [
-  {
-    title: "Student Name",
-    dataIndex: "title",
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: "Class",
-    dataIndex: "description",
-  },
-  {
-    title: "Actions",
-    dataIndex: "actions",
-    render: (text, record) => (
-      <>
-        <Button type="primary" onClick={() => handleEdit(record)}>
-          Edit
-        </Button>
-        <Popconfirm
-          title="Delete the task"
-          description="Are you sure to delete this task?"
-          onConfirm={() => handleDelete(record._id)}
-          okText="Yes"
-          cancelText="No"
-        >
-          <Button type="danger">Delete</Button>
-        </Popconfirm>
-      </>
-    ),
-  },
-];
 
 const StudentTable = ({ data, handleDelete, handleEdit }) => {
+  const columns = () => [
+    {
+      title: "Student Name",
+      dataIndex: "title",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Class",
+      dataIndex: "description",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <>
+          <Button type="primary" onClick={() => handleEdit(record)}>
+            Edit
+          </Button>
+          <Popconfirm
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            onConfirm={() => handleDelete(record._id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type="danger">Delete</Button>
+          </Popconfirm>
+        </>
+      ),
+    },
+  ];
+  
   return (
     <Table
-      columns={columns(handleDelete, handleEdit)}
+      columns={columns()}
       dataSource={data}
       rowKey="_id"
     />
